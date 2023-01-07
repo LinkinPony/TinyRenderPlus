209d83183de60aae4837f00526b5ad40da650513
LinkinPony <friendshipgame714@gmail.com>
2023-01-07 21:19:57 +0800

    Add some thirdparty lib

---
+++ main.cc
@@ -0,0 +1,13 @@
+//
+// Created by linkinpony on 23-1-7.
+//
+#include <iostream>
+#include <eigen3/Eigen/Eigen>
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust trinagulation. Requires C++11
+#define TINYOBJLOADER_USE_MAPBOX_EARCUT
+#include "ThirdPartyLib/tiny_obj_loader.h"
+using namespace Eigen;
+int main() {
+  return 0;
+}
