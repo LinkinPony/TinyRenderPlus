5b7d65b575d4f955e6a5b36fd83a4a21b0f1ceb4
LinkinPony <friendshipgame714@gmail.com>
2023-01-10 16:06:14 +0800

    update tinyObjLoader to v2

--- Render/triangle.h
+++ Render/triangle.h
@@ -4,8 +4,10 @@
 
 #ifndef TINYRENDERPLUS__TRIANGLE_H_
 #define TINYRENDERPLUS__TRIANGLE_H_
-
+#include <eigen3/Eigen/Eigen>
 class Triangle {
+  Eigen::Vector3f p1_,p2_,p3_;//Vertex of triangle
+
 
 };
 
