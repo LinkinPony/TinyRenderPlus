f0006494fd50f3f001bb72392c5c6f7789aa493f
LinkinPony <friendshipgame714@gmail.com>
2023-03-07 20:57:44 +0800

    what am I doing?

---
+++ Source/Model/texture_pool.cpp
@@ -0,0 +1,11 @@
+#include "texture_pool.h"
+
+size_t TexturePool::loadTextureByPath(const std::string& path) {
+  return size_t();
+}
+
+void TexturePool::deleteTextureByID(size_t id) {}
+
+std::shared_ptr<TGAImage> TexturePool::getTextureByID(size_t id) {
+  return std::shared_ptr<TGAImage>();
+}
